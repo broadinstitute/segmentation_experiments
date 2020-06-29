62f3dc74a3a7f2253e7adedf607945e781da972e
Eric Bueno <ericbueno20@gmail.com>
2020-06-29 11:11:35 -0400

    Edited composite function so each mask is added using linear multiplier intead of a random one.

--- composites.py
+++ composites.py
@@ -2,12 +2,12 @@ import numpy as np
 from skimage import io
 
 def composite_masks(mask_collection, loud=False):
-    composite = 0
+    composite = np.zeros(mask_collection.shape[0:2])
     
     for i in range(len(mask_collection)):
-        composite += mask_collection[i] * (5 * np.random.sample()) % 255
+        composite += (mask_collection[i] > 0) * (i+1)
     
     if loud:
-        io.imshow(composite, cmap='viridis')
+        io.imshow(composite, cmap='nipy_spectral')
     
     return composite
