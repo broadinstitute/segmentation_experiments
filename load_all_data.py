eaa3c0ba7d4e66f904439b2ddba02ffff52a288c
Eric Bueno <ericbueno20@gmail.com>
2020-07-02 13:17:14 -0400

    created function to return all png names, image paths, and mask collections in a single array

--- load_all_data.py
+++ load_all_data.py
@@ -50,55 +50,35 @@ def load_labeled_data():
 
 image_groups, table_entries, csv_lines = load_labeled_data()
 
-def load_purp_data(csv_lines=csv_lines):
+def load_COLOR_data(csv_lines=csv_lines):
+    def_png_list = []
+    def_img_paths = []
+    def_mask_colls = []
+    
+    bw_png_list = []
+    bw_img_paths = []
+    bw_mask_colls = []
+    
     purple_png_list = []
     purple_img_paths = []
     purple_mask_colls = []
     
-    for row in csv_lines:
-        if "Purple" in row:
-            purple_png_list.append(row[1])
-            
-    for png in purple_png_list:
-        img_path = training_imgs_dir + png[:-4] + "/images/" + png
-        purple_img_paths.append(img_path)
-        
-        mask_path = training_imgs_dir + png[:-4] + "/masks/*.png"
-        imcoll = io.collection.ImageCollection(mask_path)
-        purple_mask_colls.append(imcoll)
-        
-    
-    return purple_png_list, purple_img_paths, purple_mask_colls
-
-def load_pink_data(csv_lines=csv_lines):
     pink_png_list = []
     pink_img_paths = []
     pink_mask_colls = []
     
     for row in csv_lines:
-        if "Pink-Purple" in row:
+        if "Default" in row:
+            def_png_list.append(row[1])
+        elif "TissueBW" in row:
+            bw_png_list.append(row[1])
+        elif "Purple" in row:
+            purple_png_list.append(row[1])
+        elif "Pink-Purple" in row:
             pink_png_list.append(row[1])
             
-    for png in pink_png_list:
-        img_path = training_imgs_dir + png[:-4] + "/images/" + png
-        pink_img_paths.append(img_path)
-        
-        mask_path = training_imgs_dir + png[:-4] + "/masks/*.png"
-        imcoll = io.collection.ImageCollection(mask_path)
-        pink_mask_colls.append(imcoll)
+    del def_png_list[0] # first entry in the csv file is [Image, Type]
     
-    return pink_png_list, pink_img_paths, pink_mask_colls
-
-def load_default_data(csv_lines=csv_lines):
-    def_png_list = []
-    def_img_paths = []
-    def_mask_colls = []
-    
-    for row in csv_lines:
-        if "Default" or "MaybeBIG" or "SuperBig" in row:
-            def_png_list.append(row[1])
-        
-    del def_png_list[0]
     for png in def_png_list:
         img_path = training_imgs_dir + png[:-4] + "/images/" + png
         def_img_paths.append(img_path)
@@ -107,17 +87,6 @@ def load_default_data(csv_lines=csv_lines):
         imcoll = io.collection.ImageCollection(mask_path)
         def_mask_colls.append(imcoll)
         
-    return def_png_list, def_img_paths, def_mask_colls
-
-def load_bw_data(csv_lines=csv_lines):
-    bw_png_list = []
-    bw_img_paths = []
-    bw_mask_colls = []
-    
-    for row in csv_lines:
-        if "TissueBW" in row:
-            bw_png_list.append(row[1])
-
     for png in bw_png_list:
         img_path = training_imgs_dir + png[:-4] + "/images/" + png
         bw_img_paths.append(img_path)
@@ -126,5 +95,24 @@ def load_bw_data(csv_lines=csv_lines):
         imcoll = io.collection.ImageCollection(mask_path)
         bw_mask_colls.append(imcoll)
         
-    return bw_png_list, bw_img_paths, bw_mask_colls
-
+    for png in purple_png_list:
+        img_path = training_imgs_dir + png[:-4] + "/images/" + png
+        purple_img_paths.append(img_path)
+        
+        mask_path = training_imgs_dir + png[:-4] + "/masks/*.png"
+        imcoll = io.collection.ImageCollection(mask_path)
+        purple_mask_colls.append(imcoll)
+        
+    for png in pink_png_list:
+        img_path = training_imgs_dir + png[:-4] + "/images/" + png
+        pink_img_paths.append(img_path)
+        
+        mask_path = training_imgs_dir + png[:-4] + "/masks/*.png"
+        imcoll = io.collection.ImageCollection(mask_path)
+        pink_mask_colls.append(imcoll)
+    
+    all_pngs = [def_png_list, bw_png_list, purple_png_list, pink_png_list]
+    all_img_paths = [def_img_paths, bw_img_paths, purple_img_paths, pink_img_paths]
+    all_mask_colls = [def_mask_colls, bw_mask_colls, purple_mask_colls, pink_mask_colls]
+    
+    return all_pngs, all_img_paths, all_mask_colls
