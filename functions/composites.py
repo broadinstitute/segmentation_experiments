798e81716bf873fc27c6f6da63ec746dd6f3b5aa
Eric Bueno <ericbueno20@gmail.com>
2020-07-22 13:27:11 -0400

    Attempted optimizing composite function new method is 3x slower than original test displayed at end of test_01_composites.ipynb may review other solutions in the future for now, keep using original composite function

--- functions/composites.py
+++ functions/composites.py
@@ -11,3 +11,19 @@ def composite_masks(mask_collection, loud=False):
         io.imshow(composite, cmap='nipy_spectral')
     
     return composite
+
+def opt_composite_masks(mask_collection, loud=False):
+    composite = np.array(mask_collection)
+    depth = len(mask_collection)
+    end = depth + 1
+    
+    color_coef = np.arange(1, end)
+    color_coef = color_coef.reshape((depth, 1, 1))
+    
+    composite = composite * color_coef / 255. 
+    composite = np.sum(composite, axis=0)
+    
+    if loud: 
+        io.imshow(composite, cmap='nipy_spectral')
+        
+    return composite
