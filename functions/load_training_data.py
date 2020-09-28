7acf7b2b270a820ffbb5e391df2988e3815426f5
Eric Bueno <ericbueno20@gmail.com>
2020-09-28 13:44:37 -0400

    Progress on data augmentation created test_05_data_augmentation.ipynb to test new rescaling process which creates more homogeneous datasets created new function in load_training_data.py based on new technique created 04.5_train_regression.ipynb to test the regression model with the new dataset results are not as good as the initial regression test; the model needs to be modified deleted unused functions in load_training_data.py added comments to load_regression_samples()

--- functions/load_training_data.py
+++ functions/load_training_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 from functions.load_all_data import load_data_by_color, load_imgs_masks
 from functions.composites import composite_masks
@@ -112,37 +113,6 @@ def training_samples(num_samples=1000, colors=[1,1,0,0], load_masks=False):
 # returns data set with the same number of examples for each class label
 # each entry will have the cropped image, cropped mask, class label, and size
 
-def load_regression_samples(images, labels, composites):
-    """
-    Function which returns cropped images, masks, nuclei sizes, and class labels
-    
-    For each image, composite, label: 
-        takes a random crop of the image and mask
-        computes the average nuclei size in the cropped mask
-        appends results to their respective arrays
-    
-    """
-    cropped_imgs = []
-    cropped_masks = []
-    nuclei_sizes = []
-    num_samples = len(labels)
-    
-    for i in range(num_samples):
-        cropped_image, cropped_mask = random_crop(img=images[i], 
-                                                 mask_composite=composites[i],
-                                                 crop_size=64, loud=False)
-        size = compute_avg_size(cropped_mask)
-        
-        cropped_imgs.append(cropped_image)
-        cropped_masks.append(cropped_mask)
-        nuclei_sizes.append(size)
-        
-    cropped_imgs = np.array(cropped_imgs) / 255.
-    cropped_masks = np.array(cropped_masks)
-    nuclei_sizes = np.array(nuclei_sizes)
-        
-    return cropped_imgs, cropped_masks, nuclei_sizes, labels
-
 def load_rescaled_samples(num_samples=20000):
     """
     Function which returns num_samples crops of rescaled images, masks, and their nuclei sizes
@@ -179,11 +149,14 @@ def load_rescaled_samples(num_samples=20000):
     rescaled_data = np.copy(sizes_comps_imgs)
     
     print("Rescaling Data...")
-    for i in range(len(n)):
-        if n[i] != 0.:
-            target_size = bins[i]
-            for j in range(670):
-                size = sizes_comps_imgs[0][j]
+    for i in range(len(n)):                        # Go over each bin in our distribution
+        if n[i] != 0.:                             # Ignore empty bins
+            target_size = bins[i]                  # Set out target size to the current bin
+            for j in range(670):                   # Iterate over every image in our dataset
+                size = sizes_comps_imgs[0][j]      # get the size of each image for comparison 
+                
+                # Ensure that we do not index a bin which doesn't exist
+                # in either case, scale images to match their respective bins
                 if i+1 < len(n):
                     if size >= bins[i] and size < bins[i+1]:
                         rescaled_img, rescaled_comp = rescale_img_comp(sizes_comps_imgs[2][j], 
@@ -201,8 +174,8 @@ def load_rescaled_samples(num_samples=20000):
                         rescaled_data[1][j] = rescaled_comp
                         rescaled_data[2][j] = rescaled_img
     print("\tData has been rescaled.")
-    
-    def non_zero_crop(composite, image):
+        
+    def non_zero_crop(composite, image):            # This functon ensures we don't get a crop of empty space
         cropped_img, cropped_mask = random_crop(img=image, 
                                                 mask_composite=composite, 
                                                 crop_size=128)
@@ -240,3 +213,158 @@ def load_rescaled_samples(num_samples=20000):
     
     print("Done!")
     return cropped_sizes, cropped_masks, cropped_imgs
+
+def load_rescaled_samples_opt(n_samples=20, BINS=75, CROP_SIZE=128, loud=False):
+    """
+    Function which loads n_sample crops of images across bins
+    First, load all original images and masks. Composite the masks
+        and compute the average size of the nuclei in each composite
+    
+    Next, find which images are compatible with which bins
+        - compatibility means that rescaled images will still fit in a crop
+        - ensure that we don't allow images to be rescaled beyond 10 bins
+    
+    Rescale all of the images and masks based on their compatibility
+        - store rescaled data in a dictionary
+        - rescaled_data[i] --> select desired bin
+        - rescaled_data[x][i] --> select desired (size, image, composite) from bin
+        - rescaled_data[x][x][0,1,2]
+            * 0: rescaled nuclei size
+            * 1: rescaled mask composite
+            * 2: rescaled image
+    """
+    all_imgs, mask_colls = load_imgs_masks()[:2]
+    mask_comps = []
+    nuclei_sizes = []
+    
+    if loud:
+        print("Creating mask composites...")
+        start = time.time()
+    
+    for mask in mask_colls:
+        curr_comp = composite_masks(mask)
+        curr_size = compute_avg_size(curr_comp)
+
+        mask_comps.append(curr_comp)
+        nuclei_sizes.append(curr_size)
+    
+    if loud:
+        end = time.time() - start
+        print("\tComposites created in %d minutes, %.2f seconds"
+             %((end // 60), end % 60))
+    
+    # retrieve distribution of sizes
+    # only get images smaller than 3000 pixels in area 
+    n, bins, patches = plt.hist(nuclei_sizes, bins=BINS, 
+                                range=(np.min(nuclei_sizes), 3000))
+    
+    # sort all original data based on nuclei size
+    sizes_comps_imgs = np.array((nuclei_sizes, mask_comps, all_imgs), order='F')
+    ind = sizes_comps_imgs[0, :].argsort(axis=0)
+    sizes_comps_imgs[0] = sizes_comps_imgs[0][ind]
+    sizes_comps_imgs[1] = sizes_comps_imgs[1][ind]
+    sizes_comps_imgs[2] = sizes_comps_imgs[2][ind]
+    
+    # We must find out which images can be rescaled to each bin
+    # while maintaining ideal crops
+    compatible_bins = dict()
+    
+    for i in range(BINS):
+        target_size = bins[i]
+        compatible_bins[i] = []
+        
+        # iterate over every image in our dataset
+        # simulate the rescaled dimensions of the image w/ respect to the current bin
+        for j in range(669): 
+            composite = sizes_comps_imgs[1][j]
+            curr_size = sizes_comps_imgs[0][j]
+            
+            ratio = target_size / curr_size
+            ratio = ratio ** (1/2)
+            
+            # ensure rescaled image will fit inside random crop
+            if min(int(composite.shape[0] * ratio),
+                   int(composite.shape[1] * ratio)) <= CROP_SIZE:
+                continue
+            
+            # only get images that scale to the surrounding 10 bins
+            else:
+                if abs(curr_size - target_size) < (bins[1] - bins[0]) * 10:
+                    compatible_bins[i].append(j)
+
+    if loud:
+        print("Rescaling data...")
+        start = time.time()
+        
+    rescaled_data = dict()
+    
+    # iterate over every key in our compatibility dictionary
+    # rescale each compatible image to the corresponding bin
+    for key in compatible_bins:
+        target_size = bins[key]
+        rescaled_data[key] = []
+        
+        for index in compatible_bins[key]:
+            curr_comp = sizes_comps_imgs[1][index]
+            curr_img = sizes_comps_imgs[2][index]
+            
+            rescaled_img, rescaled_comp = rescale_img_comp(curr_img, curr_comp,
+                                                      target_size)
+            rescaled_size = compute_avg_size(rescaled_comp)
+
+            rescaled_data[key].append((rescaled_size, rescaled_comp, rescaled_img))
+        
+    if loud:
+        end = time.time() - start
+        print("\tData rescaled in %d minutes, %.2f seconds"
+              %((end // 60), end % 60))
+    
+    cropped_data = dict()
+    missing_samples = [n_samples for i in range(BINS)]
+    i = 0
+    cropped_data[i] = []
+    
+    if loud:
+        start = time.time()
+        print("Cropping rescaled data...")
+    
+    # Ensure that we get the same number of crops for each bin
+    while np.sum(missing_samples) != 0:
+        if missing_samples[i] != 0:
+            curr_bin = rescaled_data[i]
+            rand_index = np.random.randint(0, len(curr_bin))
+            
+            while True:
+                cropped_img, cropped_mask = random_crop(img=curr_bin[rand_index][2],
+                                                        mask_composite=curr_bin[rand_index][1],
+                                                        crop_size=CROP_SIZE)
+                cropped_size = compute_avg_size(cropped_mask)
+                
+                # ensure we don't get a crop of empty space
+                if cropped_size > 0:
+                    cropped_data[i].append((cropped_size, cropped_mask, cropped_img))
+                    break
+            
+            missing_samples[i] -= 1
+        else:
+            i += 1
+            cropped_data[i] = []
+            
+    if loud:
+        end = time.time() - start
+        print("Data cropped in %d minutes, %.2f seconds" 
+              %((end // 60), end % 60))
+        
+    cropped_sizes = []
+    cropped_comps = []
+    cropped_imgs = []
+    for key in cropped_data:
+        for triad in cropped_data[key]:
+            cropped_sizes.append(triad[0])
+            cropped_comps.append(triad[1])
+            cropped_imgs.append(triad[2])
+    
+    n, bins, patches = plt.hist(cropped_sizes, bins=BINS, 
+                                range=(np.min(cropped_sizes), 
+                                       np.max(cropped_sizes)))
+    return cropped_sizes, cropped_comps, cropped_imgs
